c0e7cf396d92b22e642d88213e8abb0f5e9ce354
Arjun Sreedharan <arjun024@gmail.com>
2014-04-15 00:35:49 +0530

    src files

---
+++ kernel.asm
@@ -0,0 +1,18 @@
+; Copyright (C) 2014  Arjun Sreedharan
+; License: GPL version 2 or higher http://www.gnu.org/licenses/gpl.html
+
+bits 32
+section .text
+		;multiboot spec
+        align 4
+        dd 0x1BADB002				;magic
+        dd 0x00    					;flags
+        dd - (0x1BADB002 + 0x00)	;checksum. m+f+c should be zero
+
+global start
+extern kmain ; this is in the c file
+
+start:
+  cli ;block interrupts
+  call kmain
+  hlt ; halt the CPU
---
+++ kernel.c
@@ -0,0 +1,28 @@
+/*
+* Copyright (C) 2014  Arjun Sreedharan
+* License: GPL version 2 or higher http://www.gnu.org/licenses/gpl.html
+*/
+
+void kmain(void)
+{
+	char *str = "my first kernel";
+	char *vidptr = (char*)0xb8000; //video mem begins here
+	unsigned int i = 0;
+	unsigned int j = 0;
+	//clear all
+	while(j < 80 * 25) {
+		vidptr[j] = ' '; //blank character
+		vidptr[j+1] = 0x07; //shows prev character should
+							 // be white on black screen
+		j = j + 2;
+	}
+	j = 0;
+	while(str[j] != '\0') {
+		vidptr[i] = str[j]; //the character's ascii
+		vidptr[i+1] = 0x07; //shows prev character should
+							 // be white on black screen
+		++j;
+		i = i + 2;
+	}
+	return;
+}
