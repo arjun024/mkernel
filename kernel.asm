07bb57f226d1f2fc08431ab07cbe303f318e95bc
Arjun Sreedharan <arjun024@gmail.com>
2014-04-15 08:40:15 +0530

    cleanup comments. better readable

--- kernel.asm
+++ kernel.asm
@@ -3,16 +3,16 @@
 
 bits 32
 section .text
-		;multiboot spec
+        ;multiboot spec
         align 4
-        dd 0x1BADB002				;magic
-        dd 0x00    					;flags
-        dd - (0x1BADB002 + 0x00)	;checksum. m+f+c should be zero
+        dd 0x1BADB002              ;magic
+        dd 0x00                    ;flags
+        dd - (0x1BADB002 + 0x00)   ;checksum. m+f+c should be zero
 
 global start
-extern kmain ; this is in the c file
+extern kmain 		;this is defined in the c file
 
 start:
-  cli ;block interrupts
+  cli 				;block interrupts
   call kmain
-  hlt ; halt the CPU
+  hlt 				;halt the CPU
