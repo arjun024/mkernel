0c07b403109ad4af75de2c00a81f69c68a0c9960
sbennett <sbennett1990@users.noreply.github.com>
2014-06-03 12:13:40 -0400

    Update kernel.c
    Fix a spelling error on line 26.
    Remove an unnecessary hyphen on line 30.

--- kernel.c
+++ kernel.c
@@ -23,11 +23,11 @@ void kmain(void)
 
 	j = 0;
 
-	//this loop write the string to video memory
+	//this loop writes the string to video memory
 	while(str[j] != '\0') {
 		//the character's ascii
 		vidptr[i] = str[j];
-		//attribute-byte: give character black bg and-light grey fg
+		//attribute-byte: give character black bg and light grey fg
 		vidptr[i+1] = 0x07;
 		++j;
 		i = i + 2;
