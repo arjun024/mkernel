3b54bf37d6a20b362325ef5074bb44fe26fa9714
Arjun Sreedharan <arjun024@gmail.com>
2015-02-03 09:18:30 +0530

    .c: refactor with gcc warning flags
    -std=c90 -Wall -Wextra -Wwrite-strings -pedantic
    
    Signed-off-by: Arjun Sreedharan <arjun024@gmail.com>

--- kernel.c
+++ kernel.c
@@ -5,35 +5,36 @@
 
 void kmain(void)
 {
-	char *str = "my first kernel";
-	// video memory begins at address 0xb8000
+	const char *str = "my first kernel";
+	/* video memory begins at address 0xb8000 */
 	char *vidptr = (char*)0xb8000;
 	unsigned int i = 0;
 	unsigned int j = 0;
 	unsigned int screensize;
 
-	// this loops clears the screen
-	// there are 25 lines each of 80 columns; each element takes 2 bytes
+	/* this loops clears the screen
+	* there are 25 lines each of 80 columns; each element takes 2 bytes */
 	screensize = 80 * 25 * 2;
 	while (j < screensize) {
-		// blank character
+		/* blank character */
 		vidptr[j] = ' ';
-		// attribute-byte
-		vidptr[j+1] = 0x07; 
+		/* attribute-byte */
+		vidptr[j+1] = 0x07;
 		j = j + 2;
 	}
 
 	j = 0;
 
-	// this loop writes the string to video memory
+	/* this loop writes the string to video memory */
 	while (str[j] != '\0') {
-		// the character's ascii
+		/* the character's ascii */
 		vidptr[i] = str[j];
-		// attribute-byte: give character black bg and light grey fg
+		/* attribute-byte: give character black bg and light grey fg */
 		vidptr[i+1] = 0x07;
 		++j;
 		i = i + 2;
 	}
-	
+
 	return;
 }
+
