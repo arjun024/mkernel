943cb5ded722631fa36e08b1a92e989fe3054462
Arjun Sreedharan <arjun024@gmail.com>
2015-02-04 13:29:02 +0530

    reserve space and set stack pointer before call
    Signed-off-by: Arjun Sreedharan <arjun024@gmail.com>

--- kernel.asm
+++ kernel.asm
@@ -14,5 +14,10 @@ extern kmain 				;this is defined in the c file
 
 start:
 	cli 				;block interrupts
+	mov esp, stack_space		;set stack pointer
 	call kmain
 	hlt 				;halt the CPU
+
+section .bss
+resb 8192				;8KB for stack
+stack_space:
