9909b52074167afe8860b0bcc5d40223fbed3599
Jacob Garby <j.garby@icloud.com>
2017-09-12 19:13:21 +0100

    Moved initialized data to the .data section
    Tested, and still works perfectly.

--- kernel.asm
+++ kernel.asm
@@ -2,13 +2,13 @@
 ; License: GPL version 2 or higher http://www.gnu.org/licenses/gpl.html
 
 bits 32
-section .text
+section .data
         ;multiboot spec
         align 4
         dd 0x1BADB002              	;magic
         dd 0x00                    	;flags
         dd - (0x1BADB002 + 0x00)   	;checksum. m+f+c should be zero
-
+section .text
 global start
 extern kmain 				;this is defined in the c file
 
